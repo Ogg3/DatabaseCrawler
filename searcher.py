673f093207fd07f9d0777765922b1b6d159201e3
Ogg3 <89068946+Ogg3@users.noreply.github.com>
2021-09-03 22:05:29 +0200

    Update searcher.py
    Cleanup and commenting

--- searcher.py
+++ searcher.py
@@ -2,14 +2,21 @@ import sqlite3
 import sys
 import os
 
-path=r'C:\Users\Forensic\Desktop\Research\DAtabase\testing\databases'
+# Set vars
+path=os.getcwd()
 list_of=[]
 
+# get files, directorys
 for root, dirs, files in os.walk(path):
     for file in files:
+        
+        # Add files to be parsed
         list_of.append(os.path.join(root,file))
 
+# Start writing html report
 with open("HTML_Report.html", "w") as f:
+    
+    # Hide elements
     JavascriptFunc="""
 <script>
 function hide(div){
@@ -24,7 +31,11 @@ function hide(div){
 </script>
 """
     f.write(JavascriptFunc)
-    for fil in list_of:    
+    
+    # Parse files
+    for fil in list_of:
+        
+        # Check if file is database
         try:
             conn = sqlite3.connect(fil)
 
@@ -36,29 +47,45 @@ function hide(div){
 
 """ % (fil,fil,fil)
             f.write(Start)
-        
+            
+            # Get structure of the database
             curs = conn.execute("SELECT sql FROM sqlite_master")
-
+            
+            # Go through query results
             for i in curs:
+                
+                # ?
                 for j in i:
+                    
+                    # ?
                     if j != None:
-                       for y in j.split(): 
-                            if 'latitude' in y or 'longitude' in y or 'location' in y:
+                       
+                        # ?
+                        for data in j.split(): 
+                            
+                            # Check for strings of interest
+                            if 'latitude' in data or 'longitude' in data or 'location' in data:
+                                
+                                # Make a div with the table name?
+                                Table_name = j.split()[2]
                                 Table_Header="""
         <tr>
             <th> %s </th>
         </tr>
 
-""" % (j.split()[2])
+""" % (Table_name)
                                 f.write(Table_Header)
+                                
+                                # Make a list of data
                                 Table_Data="""
         <tr>
             <td> %s </td>
         </tr>
-""" % (y)
+""" % (data)
                                 f.write(Table_Data)
                             
-                                     
+        # If file is not a database
+        # TODO filterout exception and only display real errors
         except Exception as e:
             print(e)
     End="""
